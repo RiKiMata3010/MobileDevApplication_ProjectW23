db8eea3d5fb760781d833a512abb2c7d0007ef26
Phoenix Armand Ani <phoenixarmand.ani@georgebrown.ca>
2023-02-21 14:08:31 -0500

    Initial Commit

---
+++ MobileDevApplication_Project/MobileDevApplication_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileDevApplication_ProjectApp.swift
+//  MobileDevApplication_Project
+//
+//  Created by Phoenix Armand Ani on 2023-02-21.
+//
+
+import SwiftUI
+
+@main
+struct MobileDevApplication_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
