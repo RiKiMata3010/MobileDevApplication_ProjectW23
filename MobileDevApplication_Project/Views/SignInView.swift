8e168128689324f91615a66b03305af8e42cf99b
Phoenix Armand Ani <phoenixarmand.ani@georgebrown.ca>
2023-04-16 15:53:52 -0400

    Made some changes

--- MobileDevApplication_Project/Views/SignInView.swift
+++ MobileDevApplication_Project/Views/SignInView.swift
@@ -12,86 +12,90 @@ struct SignInView: View {
     @Binding var currentShowingView : String
     @AppStorage("uid") var userID = ""
     
+    @State var showingLogInScreen = false
     @State var textInput : String = ""
     @State var passwordInput: String = ""
-
-// Won't use the validator so the professor can sign up or log-in
-//    private func isValidPassword(_ password: String) -> Bool{
-//        //min 6 char
-//        //1upp char
-//        //1sp char
-//        //
-//        let passwordRegex = NSPredicate(format: "SELF_MATCHES %@", "^(?=.*[a-z])(?=.*[$@$#!%*?&])(?=.*[A-Z]).{6,}$")
-//        return passwordRegex.evaluate(with: passwordInput)
-//    }
+    
+    // Won't use the validator so the professor can sign up or log-in
+    //    private func isValidPassword(_ password: String) -> Bool{
+    //        //min 6 char
+    //        //1upp char
+    //        //1sp char
+    //        //
+    //        let passwordRegex = NSPredicate(format: "SELF_MATCHES %@", "^(?=.*[a-z])(?=.*[$@$#!%*?&])(?=.*[A-Z]).{6,}$")
+    //        return passwordRegex.evaluate(with: passwordInput)
+    //    }
     
     var body: some View {
-        VStack{
-            Form{
-                HStack{
-                    Image(systemName: "mail")
-                    Spacer()
-                    TextField("Enter your Email", text: $textInput)
-                    
-                    Spacer()
-                    
-                    if(textInput.count != 0){
-                        Image(systemName: textInput.isValidEmail() ? "checkmark" : "xmark")
-                    }
-                }
-                HStack{
-                    Image(systemName: "lock")
-                    Spacer()
-                    TextField("Enter your Password", text: $passwordInput)
-                    
-                    Spacer()
-                    
-//                    if(passwordInput.count != 0){
-//                        Image(systemName: isValidPassword(passwordInput) ? "checkmark" : "xmark")
-//
-//                    }
-                }
-            }
-            Button(action:{
-                withAnimation{
-                    self.currentShowingView = "signup"
-                }
-            }){
-                Text("Don't have an Account?")
-                    .foregroundColor(.white)
-                    .font(.headline)
-                    .frame(height: 55)
-                    .frame(width: 350)
-                    .background(Color.black)
-                    .cornerRadius(10)
-            }
-            Button {
-                
-                Auth.auth().signIn(withEmail: textInput, password: passwordInput){
-                    authResult, error in
-                    
-                    if let error = error{
-                        print(error)
-                        return
-                    }
-                    if let authResult = authResult {
-                        print(authResult.user.uid)
-                        withAnimation{
-                            userID = authResult.user.uid
+            VStack{
+                Form{
+                    HStack{
+                        Image(systemName: "mail")
+                        Spacer()
+                        TextField("Enter your Email", text: $textInput)
+                        
+                        Spacer()
+                        
+                        if(textInput.count != 0){
+                            Image(systemName: textInput.isValidEmail() ? "checkmark" : "xmark")
                         }
                     }
+                    HStack{
+                        Image(systemName: "lock")
+                        Spacer()
+                        TextField("Enter your Password", text: $passwordInput)
+                        
+                        Spacer()
+                        
+                        //                    if(passwordInput.count != 0){
+                        //                        Image(systemName: isValidPassword(passwordInput) ? "checkmark" : "xmark")
+                        //
+                        //                    }
+                    }
                 }
-                
-            } label: {
-                Text("Sign In")
-                    .foregroundColor(.white)
-                    .font(.headline)
-                    .frame(height: 55)
-                    .frame(width: 350)
-                    .background(Color.black)
-                    .cornerRadius(10)
+                Button(action:{
+                    withAnimation{
+                        self.currentShowingView = "signup"
+                    }
+                }){
+                    Text("Don't have an Account?")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .frame(height: 55)
+                        .frame(width: 350)
+                        .background(Color.black)
+                        .cornerRadius(10)
+                }
+                Button {
+                    
+                    Auth.auth().signIn(withEmail: textInput, password: passwordInput){
+                        authResult, error in
+                        
+                        if let error = error{
+                            print(error)
+                            return
+                        }
+                        if let authResult = authResult {
+                            print(authResult.user.uid)
+                            withAnimation{
+                                userID = authResult.user.uid
+                            }
+                        }
+                    }
+                    
+                } label: {
+                    Text("Sign In")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .frame(height: 55)
+                        .frame(width: 350)
+                        .background(Color.black)
+                        .cornerRadius(10)
+                    NavigationLink(destination: MainListView(), label: {})
+                }
+                .navigationTitle("Log In")
+                .navigationBarHidden(true)
             }
-            .navigationTitle("Log In")
         }
     }
-}
+
